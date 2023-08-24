da070905703e54df6fb78c95fa7b28bea93e3b77
Klemens <klemens.reckfort@gmx.de>
2023-08-24 17:18:00 +0200

    Added RigidBody Base Value

---
+++ Scripts/Runtime/Base/Objects/RigidbodyObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Dubi.BaseValues
+{
+    [CreateAssetMenu(menuName = "Dubi/Base Values/Base/Rigidbody", order = 20)]
+    public class  RigidbodyObject : GenericValueObject<Rigidbody>
+    {
+        
+    }
+}
---
+++ Scripts/Runtime/Base/Values/RigidbodyValue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Dubi.BaseValues
+{
+    [System.Serializable]
+    public class RigidbodyValue : GenericBaseValue<Rigidbody, RigidbodyObject, BaseValueUpdater>
+    {
+        public RigidbodyValue(Rigidbody value) : base(value)
+        {
+        }
+
+        public void RegisterCallback(object onRigidChanged)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
